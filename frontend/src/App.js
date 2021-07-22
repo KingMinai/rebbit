aea9670c653b265ef1d4b56484a5e442c7670c73
root <root@ip-172-31-6-224.us-east-2.compute.internal>
2021-07-22 12:37:06 +0000

    fixed css not loading and changed react port to 8080

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/navbar';
